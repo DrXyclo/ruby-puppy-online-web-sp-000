c9bdd690a869aab8e7365b2ceb0fe299bdaf82f8
Dr. XyClo <sdcarmody@gmail.com>
2019-02-21 02:04:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    
+  end
+  
+  def self.clear_all
+    @@all = []
+  end 
+  
+  def self.all
+   
+    puts @@all
+  end 
+  
+end
+
+=begin 
+def self.all
+    @@all.each do |i| 
+      puts i 
+    end 
+  end 
+=end 
